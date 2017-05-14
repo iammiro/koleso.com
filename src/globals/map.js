059af9fb6d7e693b1d8023f072261df8de60e600
iammiro <miroslav.kolomiets@gmail.com>
2017-05-14 16:22:53 +0300

    add svg timer and menu

--- src/globals/map.js
+++ src/globals/map.js
@@ -5,7 +5,7 @@ export default () => {
         center: uluru,
         disableDefaultUI: true
     });
-    const img = 'img/marker.png';
+    const img = 'img/pointer.svg';
     const marker = new google.maps.Marker({
         position: uluru,
         map: map,
@@ -29,4 +29,35 @@ export default () => {
         console.log('test animation');
         TweenMax.to('.timer-pop-up-window', 1, {y: 0, ease: Circ.easeOut});
     });
+
+    $('#menu-btn').click(function () {
+        console.log('menu');
+        TweenMax.to('#menu', 1, {autoAlpha:1});
+    });
+
+    // Set the date we're counting down to
+    const countDownDate = new Date("Jan 5, 2018 23:59:59").getTime();
+
+    // Update the count down every 1 second
+    const x = setInterval(function() {
+
+        // Get todays date and time
+        const now = new Date().getTime();
+
+        // Find the distance between now an the count down date
+        const distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+        // Output the result in an element with id="demo"
+        document.getElementById("timer").innerHTML = hours + ":" + minutes;
+
+        // If the count down is over, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "EXPIRED";
+        }
+    }, 1000);
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import initMap from './globals/map'
-
+import timer from './globals/commons'
 import './blocks/index.sass';
 
 // require('offline-plugin/runtime').install();
 
 window.initMap = initMap;
+
+timer();
